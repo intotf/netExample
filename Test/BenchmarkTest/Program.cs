89df8bb13e8bee946673aae7a121b034bbe73f53
intotf <intotf@163.com>
2019-09-20 14:59:16 +0800

    Benchmark 性能测试

---
+++ Test/BenchmarkTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            BenchmarkRunner.Run<TestMd5>();
+            Console.ReadLine();
+        }
+    }
+
+    public class TestMd5
+    {
+        private string Md5(string password, int bit)
+        {
+            MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+            byte[] hashedDataBytes;
+            hashedDataBytes = md5Hasher.ComputeHash(Encoding.GetEncoding("gb2312").GetBytes(password));
+            StringBuilder tmp = new StringBuilder();
+            foreach (byte i in hashedDataBytes)
+            {
+                tmp.Append(i.ToString("x2"));
+            }
+            if (bit == 16)
+                return tmp.ToString().Substring(8, 16);
+            else
+            if (bit == 32) return tmp.ToString();//默认情况
+            else return string.Empty;
+        }
+        /// <summary>
+        /// Md5 加密
+        /// </summary>
+        [Benchmark]
+        public void TestMD5_32()
+        {
+            Md5("123456", 32);
+        }
+
+        [Benchmark]
+        public void TestMD5_16()
+        {
+            Md5("123456", 16);
+        }
+    }
+}
---
+++ Test/BenchmarkTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BenchmarkTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BenchmarkTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3cee7e07-8a4f-4d4e-a458-901c5d002013")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
