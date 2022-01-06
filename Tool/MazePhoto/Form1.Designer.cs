4200bd7b091cd4bd6c825b70945f968cce434d8c
唐锋 <intotf@163.com>
2022-01-06 14:31:30 +0800

    迷宫生成器，图像处理学习

---
+++ Tool/MazePhoto/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace MazePhoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createBtn = new System.Windows.Forms.Button();
+            this.pathfindBtn = new System.Windows.Forms.Button();
+            this.mazePanel = new System.Windows.Forms.Panel();
+            this.rowBox = new System.Windows.Forms.TextBox();
+            this.columnBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mazePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(179, 17);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(75, 23);
+            this.createBtn.TabIndex = 0;
+            this.createBtn.Text = "创建迷宫";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // pathfindBtn
+            // 
+            this.pathfindBtn.Location = new System.Drawing.Point(279, 17);
+            this.pathfindBtn.Name = "pathfindBtn";
+            this.pathfindBtn.Size = new System.Drawing.Size(75, 23);
+            this.pathfindBtn.TabIndex = 1;
+            this.pathfindBtn.Text = "自动寻路";
+            this.pathfindBtn.UseVisualStyleBackColor = true;
+            this.pathfindBtn.Click += new System.EventHandler(this.pathfindBtn_Click);
+            // 
+            // mazePanel
+            // 
+            this.mazePanel.AutoScroll = true;
+            this.mazePanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.mazePanel.Controls.Add(this.pictureBox1);
+            this.mazePanel.Location = new System.Drawing.Point(12, 59);
+            this.mazePanel.Name = "mazePanel";
+            this.mazePanel.Padding = new System.Windows.Forms.Padding(2);
+            this.mazePanel.Size = new System.Drawing.Size(611, 490);
+            this.mazePanel.TabIndex = 2;
+            // 
+            // rowBox
+            // 
+            this.rowBox.Location = new System.Drawing.Point(33, 18);
+            this.rowBox.MaxLength = 3;
+            this.rowBox.Name = "rowBox";
+            this.rowBox.Size = new System.Drawing.Size(40, 21);
+            this.rowBox.TabIndex = 3;
+            this.rowBox.Text = "20";
+            this.rowBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.intBox_KeyPress);
+            // 
+            // columnBox
+            // 
+            this.columnBox.Location = new System.Drawing.Point(109, 18);
+            this.columnBox.MaxLength = 3;
+            this.columnBox.Name = "columnBox";
+            this.columnBox.Size = new System.Drawing.Size(40, 21);
+            this.columnBox.TabIndex = 4;
+            this.columnBox.Text = "20";
+            this.columnBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.intBox_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "行";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "列";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(607, 486);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(635, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.columnBox);
+            this.Controls.Add(this.rowBox);
+            this.Controls.Add(this.mazePanel);
+            this.Controls.Add(this.pathfindBtn);
+            this.Controls.Add(this.createBtn);
+            this.Name = "Form1";
+            this.Text = "迷宫生成器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mazePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.Button pathfindBtn;
+        private System.Windows.Forms.Panel mazePanel;
+        private System.Windows.Forms.TextBox rowBox;
+        private System.Windows.Forms.TextBox columnBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Tool/MazePhoto/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MazePhoto
+{
+    public partial class Form1 : Form
+    {
+        private Random rand = new Random(); //随机数对象的实例化。不要再方法中实例化多个Random，原因见msdn
+        private PictureBox[,] map;  //存放pictureBox，迷宫格子
+        private int[,] numMap;  //保存迷宫格子的数字状态
+        private int[,] path;    //保存寻路的数组
+        private int pointX = 0; // 当前所在的位置-行，用于玩游戏时保存玩家所在的位置
+        private int pointY = 0; // 当前所在的位置-列
+        private List<Image> PathImageList = new List<Image>();  //橘黄色的背景图片
+        private List<Image> ImageList = new List<Image>();      //白色背景的个图片
+        private int width;    //迷宫的宽度-列数
+        private int height;   //迷宫的高度-行数
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //白色图片
+            ImageList.Add(global::MazePhoto.Properties.Resources._0);
+            ImageList.Add(global::MazePhoto.Properties.Resources._1);
+            ImageList.Add(global::MazePhoto.Properties.Resources._2);
+            ImageList.Add(global::MazePhoto.Properties.Resources._3);
+            ImageList.Add(global::MazePhoto.Properties.Resources._4);
+            ImageList.Add(global::MazePhoto.Properties.Resources._5);
+            ImageList.Add(global::MazePhoto.Properties.Resources._6);
+            ImageList.Add(global::MazePhoto.Properties.Resources._7);
+            ImageList.Add(global::MazePhoto.Properties.Resources._8);
+            ImageList.Add(global::MazePhoto.Properties.Resources._9);
+            ImageList.Add(global::MazePhoto.Properties.Resources._10);
+            ImageList.Add(global::MazePhoto.Properties.Resources._11);
+            ImageList.Add(global::MazePhoto.Properties.Resources._12);
+            ImageList.Add(global::MazePhoto.Properties.Resources._13);
+            ImageList.Add(global::MazePhoto.Properties.Resources._14);
+            ImageList.Add(global::MazePhoto.Properties.Resources._15);
+            //橘黄色背景的图片
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p0);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p1);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p2);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p3);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p4);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p5);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p6);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p7);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p8);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p9);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p10);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p11);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p12);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p13);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p14);
+            PathImageList.Add(global::MazePhoto.Properties.Resources.p15);
+        }
+
+        /// <summary>
+        /// 绘制pictureBox，数组数据初始化
+        /// </summary>
+        public void drawCheckerboar()
+        {
+            int currentPoint = 0;
+            int chessLength = 30;
+            int initX = 0;
+            int initY = 0;
+            int _x = initX;
+            int _y = initY;
+            PictureBox tmpBox = new PictureBox();
+            for (int x = 0; x < height * width; x++)
+            {
+                if (currentPoint % width == 0)
+                {
+                    _x = initX;
+                    _y = ((int)currentPoint / width) * chessLength + initY;
+                }
+                else
+                {
+                    _x += chessLength;
+                }
+                tmpBox = new PictureBox();
+                tmpBox.Left = _x;
+                tmpBox.Top = _y;
+                tmpBox.Width = chessLength;
+                tmpBox.Height = chessLength;
+                map[(int)currentPoint / width, currentPoint % width] = tmpBox;
+                numMap[(int)currentPoint / width, currentPoint % width] = -1;   //-1表示未遍历过
+                this.mazePanel.Controls.Add(tmpBox);
+                tmpBox = null;
+                currentPoint++;
+            }
+        }
+
+        //创建迷宫状态数据
+        private void CreateNumMap(int m, int n, int o)
+        {
+            List<int> directs = new List<int> { 0, 1, 2, 3 };   //存储未用的方向。0123分别表示上-右-下-左
+            int last = 0;
+            switch (o)
+            {
+                case 1:
+                    last = 4;
+                    break;
+                case 2:
+                    last = 8;
+                    break;
+                case 4:
+                    last = 1;
+                    break;
+                case 8:
+                    last = 2;
+                    break;
+            }
+            numMap[m, n] = last;
+            //test-begin
+            string s = "";
+            for (int f = 0; f < height; f++)
+            {
+                for (int g = 0; g < width; g++) s += numMap[f, g].ToString() + ",";
+                s += "\r\n";
+            }
+            //txt_str.Text = s;
+            //test-end
+            while (directs.Count > 0)
+            {
+                int x = m;
+                int y = n;
+                int d = rand.Next(directs.Count);
+                int t = 0;
+                switch (directs[d])
+                {
+                    case 0:
+                        x--;
+                        t = 8;
+                        break;
+                    case 1:
+                        y++;
+                        t = 4;
+                        break;
+                    case 2:
+                        x++;
+                        t = 2;
+                        break;
+                    case 3:
+                        y--;
+                        t = 1;
+                        break;
+                }
+                directs.RemoveAt(d);    //删除使用过的方向
+                if (x < height && y < width && x >= 0 && y >= 0 && numMap[x, y] == -1)//没有超出数组边界，格子未被遍历过，则为true
+                {
+                    last = t ^ last;    //异或操作
+                    numMap[m, n] = last;
+                    //test-begin
+                    string ss = "";
+                    for (int f = 0; f < height; f++)
+                    {
+                        for (int g = 0; g < width; g++) ss += numMap[f, g].ToString() + ",";
+                        ss += "\r\n";
+                    }
+                    //txt_str.Text = ss;
+                    //test-end
+                    CreateNumMap(x, y, t);  //递归操作
+                }
+            }
+        }
+
+        //生成迷宫
+        private void CreateMap()
+        {
+            numMap[0, 0] = numMap[0, 0] ^ 8;//打开一个缺口，作为进入口。
+            numMap[height - 1, width - 1] = numMap[height - 1, width - 1] ^ 2;//打开一个缺口作为出去口。
+
+            pictureBox1.Width = width * 34;
+            pictureBox1.Height = height * 34;
+            Graphics g1 = pictureBox1.CreateGraphics();//画布
+            g1.Clear(Color.White);
+
+            for (int m = 0; m < height; m++)
+            {
+                for (int n = 0; n < width; n++)
+                {
+                    int x = numMap[m, n];
+                    g1.DrawImage(ImageList[x], n * 30, m * 30);
+                    map[m, n].Image = ImageList[x];
+                }
+            }
+            g1.Dispose();
+            map[0, 0].Image = PathImageList[numMap[0, 0]]; //给第一格子换成橘黄色的背景
+        }
+
+        /// <summary>
+        /// 创建迷宫
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            height = int.Parse(this.rowBox.Text);
+            width = int.Parse(this.columnBox.Text);
+            this.mazePanel.Controls.Clear();
+            path = new int[height, width];
+            map = new PictureBox[height, width];
+            numMap = new int[height, width];
+            drawCheckerboar();
+            CreateNumMap((int)height / 2, (int)width / 2, 0);//在迷宫的中间开始遍历
+            //CreateNumMap(0, 0, 0);
+            CreateMap();
+        }
+
+        /// <summary>
+        /// 自动寻路
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private void Pathfinding(int x, int y)
+        {
+            if (x == height - 1 && y == width - 1)
+            {
+                for (int f = 0; f < height; f++)
+                {
+                    for (int g = 0; g < width; g++)
+                    {
+                        if (path[f, g] != 0)
+                        {
+                            map[f, g].Image = PathImageList[numMap[f, g]];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i <= 3; i++)
+                {
+                    int m = x;
+                    int n = y;
+                    int direct = 0;
+                    switch (i)
+                    {
+                        case 0:
+                            m--;
+                            direct = 8;
+                            break;
+                        case 1:
+                            n++;
+                            direct = 4;
+                            break;
+                        case 2:
+                            m++;
+                            direct = 2;
+                            break;
+                        case 3:
+                            n--;
+                            direct = 1;
+                            break;
+                    }
+                    if (m < height && n < width && m >= 0 && n >= 0 && (numMap[x, y] & direct) != 0 && path[m, n] == 0)
+                    {
+                        path[x, y] = 1;
+                        Pathfinding(m, n);
+                        path[x, y] = 0;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 自动寻路
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pathfindBtn_Click(object sender, EventArgs e)
+        {
+            Pathfinding(0, 0);
+        }
+
+        /// <summary>
+        /// 限制输入非数字
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void intBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != '\b' && !Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
